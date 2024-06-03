5f3aa4bf2fdeb6a38c9376ff874a9212022c07da
Ananda Muhammad Zahir <anandazahir12@gmail.com>
2024-06-03 22:01:09 +0700

    test

--- resources/views/components/form-edit-penempatan.blade.php
+++ resources/views/components/form-edit-penempatan.blade.php
@@ -1,50 +1,160 @@
-<div class="modal fade" tabindex="-1" id="edit-penempatan" aria-labelledby="edit-penempatan" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Edit Lokasi Baru</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="/penempatan">
-                    <div class="row">
-                        <div class="col-lg-6 mb-3 form-group">
-                            <label for="nomor" class="form-label">No Peti Kemas</label>
+@php
+$sentence = $data->petikemas->lokasi;
+$row="";
+$blok="";
+$tier="";
+$parts = '';
+if($sentence == 'out'|| $sentence == 'pending'){
+$parts = $sentence;
+}else{
+$parts = explode('-', $sentence);
+$row=$parts[0];
+$blok=$parts[1];
+$tier=$parts[2];
+}
+@endphp
+<form action="/penempatan" id="{{$id}}">
+    <div class="row" id="section">
+        <div class="col-lg-6 mb-3 form-group">
+            <label for="nomor" class="form-label">No Peti Kemas</label>
+            @if (( $jenis == "ekspor" && $data->pembayaran->status_cetak_spk === 'sudah cetak'))
+            <input type="text" name="id_penghubung" class="form-control" id="no_petikemas" required readonly value="  {{ $data->petikemas->no_petikemas }}">
+            @elseif (( $jenis == "impor" && $data->petikemas->status_kondisi == "available"))
+            <input type="text" name="id_penghubung" class="form-control" id="no_petikemas" required readonly value="  {{ $data->petikemas->no_petikemas }}">
+            @endif
+        </div>
+        <div class="col-lg-6 mb-3 form-group">
+            <label for="size & type" class="form-label">Size & Type:</label>
+            <input type="text" class="form-control" id="size_type" placeholder="Size & Type" name="size & type" required readonly value="{{$data->petikemas->jenis_ukuran}}">
+        </div>
 
-                            <select class="form-select" aria-label="Default select example" required>
-                                <option selected>Plih Opsi Ini</option>
-                                <option value="impor">Peti Kemas A</option>
-                                <option value="ekspor">Peti Kemas B</option>
-                            </select>
-                        </div>
-                        <div class="col-lg-6 mb-3 form-group">
-                            <label for="size" class="form-label">Lokasi </label>
-                            <input type="text" class="form-control" id="lokasi" placeholder="Lokasi"
-                                name="lokasi" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6 mb-3 form-group">
-                            <label for="size & type" class="form-label">Size & Type:</label>
-                            <input type="text" class="form-control" id="size & type" placeholder="Size & Type"
-                                name="size & type" required>
-                        </div>
-                        <div class="col-lg-6 mb-3 form-group">
-                            <label for="tanggal penempatan" class="form-label">Tanggal Penempatan</label>
-                            <input type="date" class="form-control" name="tanggal-penempatan" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12 mb-3 form-group">
-                            <label for="operator alat berat" class="form-label">Operator alat berat </label>
-                            <input type="text" class="form-control" id="operatoralatberat"
-                                placeholder="Operator Alat Berat" name="operator alat berat" required>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary text-white" data-bs-toggle="modal"
-                        data-bs-target="#toastModal">Submit</button>
-                </form>
-            </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 mb-3 form-group">
+            <label for="operator alat berat" class="form-label">Operator alat berat </label>
+            <input type="text" class="form-control" id="operatoralatberat" placeholder="Operator Alat Berat" name="operator alat berat" required />
+        </div>
+        <div class="col-lg-6 mb-3 form-group">
+            <label for="operator alat berat" class="form-label">Operator alat berat </label>
+            <input type="text" class="form-control" id="operatoralatberat" placeholder="Operator Alat Berat" name="operator alat berat" required />
         </div>
     </div>
-</div>
+    <div class="row">
+        <label for="nomor" class="form-label">Lokasi Peti Kemas</label>
+        <div class="col-lg-4 mb-3 form-group">
+            <label for="nomor" class="form-label">Row</label>
+            <select class="form-select" name="row" id="row" required>
+                <option selected disabled>Plih Opsi Ini</option>
+                <option value="A1" {{ $row == 'A1' ? 'selected' : '' }}>A1</option>
+                <option value="pending" {{ $parts == 'pending' ? 'selected' : '' }}>Pending</option>
+                <option value="out" {{ $parts == 'out' ? 'selected' : '' }}>Out</option>
+            </select>
+        </div>
+        <div class="col-lg-4 mb-3 form-group">
+            <label for="blok" class="form-label">Blok</label>
+            <select class="form-select" name="blok" id="blok" required>
+                <option selected disabled>Plih Opsi Ini</option>
+                <option value="11" {{ $blok == '11' ? 'selected' : '' }}>11</option>
+                <option value="pending" {{ $parts == 'pending' ? 'selected' : '' }}>Pending</option>
+                <option value="out" {{ $parts == 'out' ? 'selected' : '' }}>Out</option>
+            </select>
+        </div>
+        <div class="col-lg-4 mb-3 form-group">
+            <label for="nomor" class="form-label">Tier</label>
+            <select class="form-select" name="tier" id="tier" required>
+                <option selected disabled>Plih Opsi Ini</option>
+                <option value="11" {{ $tier == '11' ? 'selected' : '' }}>11</option>
+                <option value="pending" {{ $parts == 'pending' ? 'selected' : '' }}>Pending</option>
+                <option value="out" {{ $parts == 'out' ? 'selected' : '' }}>Out</option>
+            </select>
+        </div>
+
+    </div>
+    <button type="submit" class="btn btn-primary text-white">Submit</button>
+</form>
+<script>
+    $(document).ready(function() {
+        if ($("#row").find("option:selected").val() == "pending" || $("#row").find("option:selected").val() == "out") {
+            $("#row").parent().removeClass("col-lg-4");
+            $("#row").parent().addClass("col-lg-12");
+            $("#tier").siblings("label").hide();
+            $("#blok").siblings("label").hide();
+            $("#tier").hide();
+            $("#blok").hide();
+        }
+
+        $("#row").on("change", function() {
+            console.log("hai");
+            $(this).parent().removeClass("col-lg-12");
+            $(this).parent().addClass("col-lg-4");
+            $("#tier").show();
+            $("#blok").show();
+            $("#tier").siblings("label").show();
+            $("#blok").siblings("label").show();
+            if ($("#tier").val() == "pending" || $("#tier").val() == "out") {
+                $("#tier").find('option[disabled]').prop("selected", true);
+                $("#blok").find('option[disabled]').prop("selected", true);
+            }
+            var value = $(this).val();
+            if (value == "pending" || value == "out") {
+                $("#tier").hide();
+                $("#blok").hide();
+                $("#tier").siblings("label").hide();
+                $("#blok").siblings("label").hide();
+                $(this).parent().removeClass("col-lg-4");
+                $(this).parent().addClass("col-lg-12");
+                $("#tier").find('option[value="' + value + '"]').prop("selected", true);
+                $("#blok").find('option[value="' + value + '"]').prop("selected", true);
+            }
+            console.log($("#blok").val());
+        });
+        $("#tier").on("change", function() {
+            $(this).parent().removeClass("col-lg-12");
+            $(this).parent().addClass("col-lg-4");
+            $("#row").show();
+            $("#blok").show();
+            $("#row").siblings("label").show();
+            $("#blok").siblings("label").show();
+            if ($("#row").val() == "pending" || $("#row").val() == "out") {
+                $("#row").find('option[disabled]').prop("selected", true);
+                $("#blok").find('option[disabled]').prop("selected", true);
+            }
+            var value = $(this).val();
+            if (value == "pending" || value == "out") {
+                $("#row").hide();
+                $("#blok").hide();
+                $("#row").siblings("label").hide();
+                $("#blok").siblings("label").hide();
+                $(this).parent().removeClass("col-lg-4");
+                $(this).parent().addClass("col-lg-12");
+                $("#row").find('option[value="' + value + '"]').prop("selected", true);
+                $("#blok").find('option[value="' + value + '"]').prop("selected", true);
+            }
+        });
+        $("#blok").on("change", function() {
+            $(this).parent().removeClass("col-lg-12");
+            $(this).parent().addClass("col-lg-4");
+            $("#row").show();
+            $("#tier").show();
+            $("#row").siblings("label").show();
+            $("#tier").siblings("label").show();
+            if ($("#row").val() == "pending" || $("#row").val() == "out") {
+                $("#row").find('option[disabled]').prop("selected", true);
+                $("#tier").find('option[disabled]').prop("selected", true);
+            }
+            var value = $(this).val();
+            if (value == "pending" || value == "out") {
+                $("#row").hide();
+                $("#tier").hide();
+                $("#row").siblings("label").hide();
+                $("#tier").siblings("label").hide();
+                $(this).parent().removeClass("col-lg-4");
+                $(this).parent().addClass("col-lg-12");
+                $("#row").find('option[value="' + value + '"]').prop("selected", true);
+                $("#tier").find('option[value="' + value + '"]').prop("selected", true);
+            }
+        });
+
+
+    });
+</script>
--- resources/views/components/table-penempatan.blade.php
+++ resources/views/components/table-penempatan.blade.php
@@ -1,97 +1,88 @@
 <div class="bg-primary rounded-4 shadow p-3 mb-3 position-relative" style="height: auto;">
-    <div class=" container ">
-        <div class="row justify-content-between p-0 m-0">
-            <h2 class="text-white fw-semibold col-lg-9 m-0 p-0">Penempatan</h2>
-            <button class="btn btn-info p-1 col-lg-2 mt-3 mt-lg-0" data-bs-toggle="modal" data-bs-target="#create-penempatan">
-                <div class="d-flex gap-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Menambah lokasi baru">
-                    <i class="fa-solid fa-circle-plus text-white fa-xl" style="margin-top:11px"></i>
-                    <span class="fw-semibold fs-6">Tambah Lokasi Baru</span>
+    <div class="bg-white mt-3 p-2 rounded-4 shadow onscroll table-responsive" style="height: 25rem;">
 
-                </div>
-            </button>
-        </div>
-
-        <div class="bg-white mt-3 p-2 rounded-4 shadow onscroll table-responsive" style="height: 25rem;">
-            <table class="table-variations-3  text-center">
-                <thead>
-                    <tr>
-                        @foreach ($data->penghubungs as $penghubung)
-                        @if ($data->jenis_kegiatan == "ekspor" && $penghubung->pembayaran->status_cetak_spk === 'sudah cetak')
-                        <th scope="col" class="fw-semibold">No Peti Kemas</th>
-                        <th scope="col" class="fw-semibold">Size & Type</th>
-                        <th scope="col" class="fw-semibold">Lokasi</th>
-                        <th scope="col" class="fw-semibold">Status Ketersediaan</th>
-                        @break
-                        @elseif ($data->jenis_kegiatan == "impor" && $penghubung->pengecekan->survey_in && $penghubung->petikemas->status_kondisi == "available")
-                        <th scope="col" class="fw-semibold">No Peti Kemas</th>
-                        <th scope="col" class="fw-semibold">Size & Type</th>
-                        <th scope="col" class="fw-semibold">Lokasi</th>
-                        <th scope="col" class="fw-semibold">Status Ketersediaan</th>
-                        @break
-                        @endif
-                        @endforeach
-                        @foreach ($data->penghubungs as $penghubung)
-                        @if ($penghubung->perbaikan->tally)
-                        <th scope="col" class="fw-semibold">Tanggal Penempatan</th>
-                        <th scope="col" class="fw-semibold">Tally</th>
-                        <th scope="col" class="fw-semibold">Operator Berat</th>
-                        <th scope="col"></th>
-                        @break
-                        @endif
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
+        <table class="table-variations-3  text-center">
+            <thead>
+                <tr>
                     @foreach ($data->penghubungs as $penghubung)
-                    @if (($data->jenis_kegiatan == "ekspor" && $penghubung->pembayaran->status_cetak_spk === 'sudah cetak') || ($data->jenis_kegiatan == "impor" && $penghubung->pengecekan->survey_in && $penghubung->petikemas->status_kondisi == "available"))
-                    <tr>
-
-                        <td class="text-center">
-                            {{$penghubung->petikemas->no_petikemas}}
-                        </td>
-                        <td class="text-center">
-                            {{$penghubung->petikemas->jenis_ukuran}}
-                        </td>
-                        <td class="text-center">
-                            <span class="{{ $penghubung->petikemas->lokasi == 'pending' ? 'bg-warning' : 'bg-danger' }} p-1 rounded-2 text-white">
-                                {{$penghubung->petikemas->lokasi}}
-                            </span>
-                        </td>
-                        <td>
-                            <span class="{{ $penghubung->petikemas->status_ketersediaan == 'in' ? 'bg-success' : 'bg-danger' }} p-1 rounded-2 text-white">
-                                {{$penghubung->petikemas->status_ketersediaan}}
-                            </span>
-                        </td>
-                        @if ($penghubung->penempatan->tally)
-                        <td class="text-center">
-                            {{$penghubung->penempatan->tanggal_penempatan}}
-                        </td>
-                        <td class="text-center d-flex gap-1">
-                            <i class="fa-solid fa-circle-user text-primary my-2 d-none d-lg-block"></i>
-                            <span>{{$penghubung->penempatan->tally}}</span>
-                        </td>
-                        <td class="text-center">
-                            <div class="d-flex gap-1">
-                                <i class="fa-solid fa-circle-user text-primary my-2 d-none d-lg-block"></i>
-                                <span>{{$penghubung->penempatan->operator_alat_berat}}</span>
-                            </div>
-                        </td>
-                        <td class="text-center">
-                            <div class="btn-group gap-2 mx-auto">
-                                <div class="d-flex gap-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Mengubah data penempatan">
-                                    <button class="btn btn-info text-white rounded-3" data-bs-toggle="modal" data-bs-target="#edit-penempatan"> <i class="fa-solid fa-pen-to-square fa-lg my-1"></i></button>
-                                </div>
-                            </div>
-                        </td>
-                        @endif
-                    </tr>
+                    @if ($data->jenis_kegiatan == "ekspor" && $penghubung->pembayaran->status_cetak_spk === 'sudah cetak')
+                    <th scope="col" class="fw-semibold">No Peti Kemas</th>
+                    <th scope="col" class="fw-semibold">Size & Type</th>
+                    <th scope="col" class="fw-semibold">Lokasi</th>
+                    <th scope="col" class="fw-semibold">Status Ketersediaan</th>
+                    @break
+                    @elseif ($data->jenis_kegiatan == "impor" && $penghubung->pengecekan->survey_in && $penghubung->petikemas->status_kondisi == "available")
+                    <th scope="col" class="fw-semibold">No Peti Kemas</th>
+                    <th scope="col" class="fw-semibold">Size & Type</th>
+                    <th scope="col" class="fw-semibold">Lokasi</th>
+                    <th scope="col" class="fw-semibold">Status Ketersediaan</th>
+                    @break
                     @endif
                     @endforeach
-                </tbody>
-            </table>
-        </div>
+                    @foreach ($data->penghubungs as $penghubung)
+                    @if ($penghubung->penempatan->tally)
+                    <th scope="col" class="fw-semibold">Tanggal Penempatan</th>
+                    <th scope="col" class="fw-semibold">Tally</th>
+                    <th scope="col" class="fw-semibold">Operator Berat</th>
+                    @break
+                    @endif
+                    @endforeach
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data->penghubungs as $penghubung)
+                @if (($data->jenis_kegiatan == "ekspor" && $penghubung->pembayaran->status_cetak_spk === 'sudah cetak') || ($data->jenis_kegiatan == "impor" && $penghubung->pengecekan->survey_in && $penghubung->petikemas->status_kondisi == "available"))
+                <tr>
+
+                    <td class="text-center">
+                        {{$penghubung->petikemas->no_petikemas}}
+                    </td>
+                    <td class="text-center">
+                        {{$penghubung->petikemas->jenis_ukuran}}
+                    </td>
+                    <td class="text-center">
+                        <span class="{{ $penghubung->petikemas->lokasi == 'pending' ? 'bg-warning' : 'bg-danger' }} p-1 rounded-2 text-white">
+                            {{$penghubung->petikemas->lokasi}}
+                        </span>
+                    </td>
+                    <td>
+                        <span class="{{ $penghubung->petikemas->status_ketersediaan == 'in' ? 'bg-success' : 'bg-danger' }} p-1 rounded-2 text-white">
+                            {{$penghubung->petikemas->status_ketersediaan}}
+                        </span>
+                    </td>
+                    @if ($penghubung->penempatan->tally)
+                    <td class="text-center">
+                        {{$penghubung->penempatan->tanggal_penempatan}}
+                    </td>
+                    <td class="text-center d-flex gap-1">
+                        <i class="fa-solid fa-circle-user text-primary my-2 d-none d-lg-block"></i>
+                        <span>{{$penghubung->penempatan->tally}}</span>
+                    </td>
+                    <td class="text-center">
+                        <div class="d-flex gap-1">
+                            <i class="fa-solid fa-circle-user text-primary my-2 d-none d-lg-block"></i>
+                            <span>{{$penghubung->penempatan->operator_alat_berat}}</span>
+                        </div>
+                    </td>
+                    @endif
+                    <td class="text-center">
+                        <div class="btn-group gap-2 mx-auto">
+                            <div class="d-flex gap-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Mengubah data penempatan">
+                                <button class="btn btn-info text-white rounded-3" data-bs-toggle="modal" data-bs-target="#edit-penempatan-{{$penghubung->penempatan->id}}"> <i class="fa-solid fa-pen-to-square fa-lg my-1"></i></button>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                @endif
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
-<x-modal-form size="modal-xl" text="Form Penempatan" id="create-penempatan">
-    <x-form-create-penempatan :data="$data" />
+</div>
+@foreach ($data->penghubungs as $item)
+<x-modal-form size="modal-xl" text="Form Edit Penempatan" id="edit-penempatan-{{$item->penempatan->id}}">
+    <x-form-edit-penempatan :data="$item" jenis="{{$data->jenis_kegiatan}}" id="form-edit-penempatan-{{$item->penempatan->id}}" />
 </x-modal-form>
+@endforeach
